58342ec679304e4a8e53b71eeba0e5068e087fe2
Jaleed Ur Rehman <jaleed12@me.com>
2019-02-09 10:47:40 -0800

    added second activity

--- app/src/main/java/comp3717/bcit/ca/ilovenwest/Homepage.java
+++ app/src/main/java/comp3717/bcit/ca/ilovenwest/Homepage.java
@@ -1,7 +1,9 @@
 package comp3717.bcit.ca.ilovenwest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Homepage extends Activity {
 
@@ -9,5 +11,10 @@ public class Homepage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+
+    protected void onClickCreatePlan(View v) {
+        Intent i = new Intent(this, ChooseActivity.class);
+        startActivity(i);
     }
 }
