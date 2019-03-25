aa43097b8f19b834d16265f2482030437953f98d
David Lu <32254699+formidable255@users.noreply.github.com>
2019-03-24 22:32:56 -0700

    adde empty long click function

--- app/src/main/java/comp3717/bcit/ca/ilovenwest/ViewPlans.java
+++ app/src/main/java/comp3717/bcit/ca/ilovenwest/ViewPlans.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -54,6 +56,13 @@ public class ViewPlans extends AppCompatActivity {
                         Plan plan = planList.get(position);
                         i.putExtra("plan", plan);
                         startActivity(i);
+                    }
+                });
+                lvPlans.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+                    @Override
+                    public boolean onItemLongClick(AdapterView parent, View view, int position, long id) {
+
+                        return true;
                     }
                 });
             }
