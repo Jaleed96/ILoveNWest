af2d8390e9f2abd21e6130d7b650a6d92674bd4d
Jaleed Ur Rehman <jaleed12@me.com>
2019-03-27 01:27:01 -0700

    updated UI for login page

--- app/src/main/java/comp3717/bcit/ca/ilovenwest/ViewEventDetails.java
+++ app/src/main/java/comp3717/bcit/ca/ilovenwest/ViewEventDetails.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ViewEventDetails extends AppCompatActivity {
@@ -21,7 +22,28 @@ public class ViewEventDetails extends AppCompatActivity {
         eventNameTv.setText(curEvent.getName());
 
         TextView eventDescriptionTv = findViewById(R.id.eventDesc);
-        eventDescriptionTv.setText(curEvent.getDescription());
+        if (curEvent.getDescription() == null || curEvent.getDescription().equals("")) {
+            eventDescriptionTv.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque eu. Eget nunc lobortis mattis aliquam faucibus purus in massa. Donec ac odio tempor orci dapibus ultrices. Proin fermentum leo vel orci porta non pulvinar. Pharetra convallis posuere morbi leo urna. Volutpat ac tincidunt vitae semper quis lectus nulla at. Massa enim nec dui nunc mattis enim. Aliquet lectus proin nibh nisl condimentum id venenatis a. Eu facilisis sed odio morbi quis.");
+        } else {
+            eventDescriptionTv.setText(curEvent.getDescription());
+        }
+        ImageView image = findViewById(R.id.categoryImage);
+        switch (curEvent.getCategory()) {
+            case "Events":
+                image.setImageResource(R.drawable.firecracker);
+                break;
+            case "Heritage":
+                image.setImageResource(R.drawable.parchment);
+                break;
+            case "Public Art/Monuments":
+                image.setImageResource(R.drawable.palette);
+                break;
+            case "Venues":
+                image.setImageResource(R.drawable.violin);
+                break;
+            default:
+                break;
+        }
     }
 
     public void onClickGotoMaps(View v) {
